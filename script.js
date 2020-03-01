c548c7b484bf02be81e03bc4eeaf4e259fbe93ca
Esmail <elmoussel12@gmail.com>
2020-03-01 04:45:46 +0200

    Adding some comments!

--- script.js
+++ script.js
@@ -3,16 +3,18 @@ var enterBtn = document.getElementById('enterBtn');
 var ul = document.querySelector('ul');
 var reset = document.getElementById('reset');
 
+// ----------- To get previous items when reload the page -------------
 for (let i = 0; i<localStorage.length ; i++)
 {
     var li = document.createElement('li');
     var deleteBtn = document.createElement('button');
-    li.innerHTML = localStorage.getItem(localStorage.key(i));
+    li.innerHTML = localStorage.getItem(localStorage.key(i)); //Grap previous item's value from LOCAL STORAGE
     deleteBtn.innerHTML = 'Delete';
     deleteBtn.onclick = deleting;
     li.appendChild(deleteBtn);
     ul.appendChild(li);
 }
+
 function addingToList(){
     var li = document.createElement('li');
     var deleteBtn = document.createElement('button');
@@ -21,31 +23,24 @@ function addingToList(){
     deleteBtn.onclick = deleting;
     li.appendChild(deleteBtn);
     ul.appendChild(li);
-    localStorage.setItem(userInput.value+'<button>Delete</button>' , userInput.value);
+    localStorage.setItem(userInput.value+'<button>Delete</button>' , userInput.value); //Set item in local storage and adding that text to the key to be able to get it and delete it easliy..
     userInput.value = '';
 }
 function clicking(){
-    if(userInput.value)
-    {
+    if(userInput.value)  //checking if the input is empty or not!
         addingToList();
-    }
 }
 function pressing(){
-    if(userInput.value && event.keyCode === 13)
-    {
+    if(userInput.value && event.keyCode === 13)  //checking if the input is empty or not and make Enter button on the keyboard working!
         addingToList();
-    }  
 }
 function deleting(){
-    localStorage.removeItem(this.parentElement.innerHTML);
-    this.parentElement.remove();
+    localStorage.removeItem(this.parentElement.innerHTML); //remove items from the localstorage!
+    this.parentElement.remove();    //remove from the list!
 }
-
-
-reset.onclick = function(){
+reset.onclick = function(){  //to reset the local storage and start over!
     localStorage.clear();
     location.reload();
 }
-
 enterBtn.addEventListener('click',clicking);
 userInput.addEventListener('keypress',pressing);
